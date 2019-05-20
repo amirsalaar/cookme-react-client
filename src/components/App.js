9556a4f53268165725cdd3636c825a821d36147f
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-20 02:30:15 -0700

    Add Navbar and Add route for pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import NavBar from './NavBar';
+import SignUpPage from './SignUpPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         </header>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/sign-up" component={SignUpPage} />
         </Switch>
       </div>
     </BrowserRouter>
