40bb5a10d0442eda591deb7d51ac0ae887692d99
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-22 01:55:58 -0700

    Style cooks sign up page

--- src/components/SignUpCooks.js
+++ src/components/SignUpCooks.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { User } from '../api/user';
+import { Icon, Button, Checkbox, Form, Input, Radio, Select, TextArea } from 'semantic-ui-react';
 
 export default function SignUpCooks(props) {
   const { onSignUp } = props;
@@ -26,33 +27,47 @@ export default function SignUpCooks(props) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="first_name"> First Name </label>
-          <input type="text" name="first_name" id="first_name" />
-        </div>
-        <div>
-          <label htmlFor="last_name"> Last Name </label>
-          <input type="text" name="last_name" id="last_name" />
-        </div>
-        <div>
-          <label htmlFor="email"> Email </label>
-          <input type="text" name="email" id="email" />
-        </div>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Field name='first_name' control={Input} label='First Name' placeholder='First Name' required />
+            <Form.Field control={Input} label='Last Name' placeholder='Last Name' required />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Input placeholder='Street Address' width={7} required />
+            <Form.Input placeholder='City' width={4} required />
+            <Form.Input control={Select} placeholder='Province' width={3} required />
+            <Form.Input placeholder='Postal Code' width={2} required />
+          </Form.Group>
+
+          <Form.Group widths='equal'>
+            <Form.Field required width={7}>
+              <label htmlFor="email">Email Address</label>
+              <Input type='email' name='email' iconPosition='left' size='small' placeholder='Email Address'>
+                <Icon name='at' />
+                <input />
+              </Input>
+            </Form.Field>
+            <Form.Field name='password' label='Password' control={Input} width={7} type='password' placeholder='Password' required />
+            <Form.Field name='password_confirmation' label='Password Confirmation' control={Input} width={7} type='password' placeholder='Password Confrimation' required />
+          </Form.Group>
+
+        </Form>
         <div>
           <label htmlFor="certificate"> Cooking Certificate </label>
           <input type="file" name="certificate" id="certificate" />
         </div>
         <div>
           <label htmlFor="address"> Kitchen Address </label>
-          <input type="text" name="address" id="address" />
+          <input type="text" name="address" id="address" required />
         </div>
         <div>
           <label htmlFor="password"> Password </label>
-          <input type="text" name="password" id="password" />
+          <input type="password" name="password" id="password" required />
         </div>
         <div>
           <label htmlFor="password_confirmation"> Password Confirmation </label>
-          <input type="text" name="password_confirmation" id="password_confirmation" />
+          <input type="password" name="password_confirmation" id="password_confirmation" required />
         </div>
         <input type="submit" value="Sign Up" />
       </form>
--- src/components/SignUpCustomers.js
+++ src/components/SignUpCustomers.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { User } from '../api/user';
+import { Icon, Input } from 'semantic-ui-react';
 
 export default function SignUpCustomers(props) {
   const { onSignUp } = props;
@@ -28,23 +29,23 @@ export default function SignUpCustomers(props) {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="first_name"> First Name </label>
-          <input type="text" name="first_name" id="first_name" />
+          <input type="text" name="first_name" id="first_name" required />
         </div>
         <div>
           <label htmlFor="last_name"> Last Name </label>
-          <input type="text" name="last_name" id="last_name" />
+          <input type="text" name="last_name" id="last_name" required />
         </div>
         <div>
           <label htmlFor="email"> Email </label>
-          <input type="text" name="email" id="email" />
+          <input type="email" name="email" id="email" required />
         </div>
         <div>
           <label htmlFor="password"> Password </label>
-          <input type="text" name="password" id="password" />
+          <input type="password" name="password" id="password" required />
         </div>
         <div>
           <label htmlFor="password_confirmation"> Password Confirmation </label>
-          <input type="text" name="password_confirmation" id="password_confirmation" />
+          <input type="password" name="password_confirmation" id="password_confirmation" required />
         </div>
         <input type="submit" value="Sign Up" />
       </form>
