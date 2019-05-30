cd4ca4707188f003aa21d49789b4bd54f4869bb6
Amirsalar Heydari <amirsalaar@gmail.com>
2019-05-30 03:20:13 -0700

    Debug not returning a rounded float

--- src/components/CheckoutSidebar.js
+++ src/components/CheckoutSidebar.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Card, Button, Icon, List, Image, Grid } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 
+const classes = {
+  sidebarCardHeader: { fontSize: 15 },
+  listQuantity: { marginRight: 5, marginLeft: 3, fontWeight: 'bold' },
+  itemName: { display: 'flex' },
+  totalList: { fontSize: '0.8 em' },
+};
+
 class CheckoutSidebar extends Component {
   state = {
     orders: [],
@@ -39,12 +46,7 @@ class CheckoutSidebar extends Component {
 
   render() {
     const { orders: cartItems } = this.state;
-    const classes = {
-      sidebarCardHeader: { fontSize: 15 },
-      listQuantity: { marginRight: 5, marginLeft: 3, fontWeight: 'bold' },
-      itemName: { display: 'flex' },
-      totalList: { fontSize: '0.8 em' },
-    };
+
     return (
       <Card centered fluid>
         <Card.Content>
@@ -85,7 +87,7 @@ class CheckoutSidebar extends Component {
               <List.Content floated='left'>
                 Total <small>(Inc. Tax)</small>
               </List.Content>
-              <List.Content floated='right'><Icon name='dollar' size='small' />{parseFloat(this.state.subTotal) + parseFloat(this.state.tax)}</List.Content>
+              <List.Content floated='right'><Icon name='dollar' size='small' />{(parseFloat(this.state.subTotal) + parseFloat(this.state.tax)).toFixed(2)}</List.Content>
             </List.Item>
           </List>
         </Card.Content>
