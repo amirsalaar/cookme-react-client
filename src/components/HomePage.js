7c4bcac7c60bb5f5171df91ea67277dca76d8094
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-01 03:46:45 -0700

    Install PropType package, Implement Search method for foods

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Input, GridRow } from 'semantic-ui-react';
+import SearchBox from './SearchBox';
 
 export default function HomePage() {
   document.body.className = ('home-page')
@@ -9,13 +10,15 @@ export default function HomePage() {
         < Grid className='home' verticalAlign='middle' centered >
           <GridRow>
             <Grid.Column mobile={12} tablet={10} computer={8} largeScreen={11} stretched>
-              <h1 class="hero-header">Home cooks get paid to feed you.</h1>
+              <h1 className="hero-header">Home cooks get paid to feed you.</h1>
               {/* <h2 className="hero-tagline">Let home cooks feed you.</h2> */}
             </Grid.Column>
           </GridRow>
           <GridRow>
             <Grid.Column mobile={12} tablet={10} computer={8} largeScreen={6} stretched >
-              <Input size='huge' action={{ icon: 'search' }} placeholder='Nearby Kitchens...' />
+              <SearchBox
+                
+              />
             </Grid.Column>
           </GridRow>
         </Grid >
---
+++ src/components/SearchBox.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import { Input, Grid, Header, Segment, Search, Label, Container, Icon, Image, GridColumn } from 'semantic-ui-react';
+import _ from 'lodash';
+import PropTypes from 'prop-types'
+import Food from '../api/food';
+
+let source = [];
+
+const initialState = {
+  isLoading: false,
+  results: [],
+  value: '',
+};
+const styles = {
+  description: { fontSize: 14, color: 'grey' },
+  price: { fontSize: 14, color: 'green' },
+  name: { marginBottom: 5 },
+  picture: { margin: 'auto' }
+};
+
+const resultRenderer = ({ name, description, price, pictures }) => {
+  return (
+    <Container>
+      <Grid>
+        <Grid.Column width={11} floated='left'>
+          <Header as='h4' content={name} style={styles.name} />
+          {description && <p style={styles.description}>{description}</p>}
+        </Grid.Column>
+        <GridColumn width={3} style={styles.picture}>
+          {pictures && <Image src={pictures[0].url} />}
+
+        </GridColumn>
+        <Grid.Column
+          width={2}
+          floated='right'
+          textAlign='right'
+          style={styles.price}
+          verticalAlign='middle'
+        >
+          {price &&
+            <>
+              <Icon name='dollar' size='small' /> {price}
+            </>
+          }
+
+        </Grid.Column>
+      </Grid>
+    </Container>
+  )
+}
+
+resultRenderer.propTypes = {
+  name: PropTypes.string,
+  description: PropTypes.string,
+  pictures: PropTypes.array,
+  price: PropTypes.string
+}
+
+
+export default class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  };
+
+  handleResultSelect = (e, { result }) => {
+    this.setState({ value: result.name });
+
+  };
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.setState(initialState)
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.name)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(source, isMatch),
+      })
+    }, 300)
+  };
+
+  async componentDidMount() {
+    await Food.all().then(foods => {
+      source = foods
+    });
+  };
+
+  render() {
+    const { isLoading, value, results } = this.state
+
+    return (
+      <Search
+        fluid
+        loading={isLoading}
+        onResultSelect={this.handleResultSelect}
+        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+          leading: true,
+        })}
+        resultRenderer={resultRenderer}
+        results={results}
+        value={value}
+        {...this.props}
+        size='huge'
+      />
+    )
+  }
+}
