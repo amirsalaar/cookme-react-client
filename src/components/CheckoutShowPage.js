1f68798890fc2a40fcb8b698679af7a2fcd98991
Amirsalar Heydari <amirsalaar@gmail.com>
2019-06-02 17:44:11 -0700

    Add footer to checkout page

--- src/components/CheckoutShowPage.js
+++ src/components/CheckoutShowPage.js
@@ -5,6 +5,7 @@ import { Elements, StripeProvider } from 'react-stripe-elements';
 import { Step } from 'semantic-ui-react';
 import { Order } from '../api/order';
 import Receipt from './Receipt';
+import Footer from './Footer';
 
 const styles = {
   itemsContainer: { width: '60%', marginBottom: '2em', marginTop: '2em' },
@@ -86,168 +87,178 @@ export default class CheckoutShowPage extends Component {
       orderID
     };
     return (
-      <Container mobile={16} tablet={10} computer={9}>
-        <Grid stackable centered >
+      <>
+        <div className='page checkout'>
+          <Container mobile={16} tablet={10} computer={9}>
+            <Grid
+              stackable
+              centered
+              verticalAlign='center'
+              style={{paddingTop: '5em'}}
+            >
 
-          <Grid.Row centered>
-            <Grid.Column mobile={16} tablet={15} computer={13}>
+              <Grid.Row centered>
+                <Grid.Column mobile={16} tablet={15} computer={13}>
 
-              <Step.Group size='tiny' attached>
-                <Step
-                  // active={active === 'Confirmation'}
-                  disabled={stage === 'Confirmation' ? false : true}
-                  icon='info'
-                  link
-                  onClick={this.handleClick}
-                  title='Confirm Order'
-                  description='Choose your shipping options'
-                />
-                <Step
-                  // active={active === 'Billing'}
-                  disabled={stage === 'Billing' ? false : true}
-                  icon='credit card'
-                  link
-                  onClick={this.handleClick}
-                  title='Billing'
-                  description='Download your receipt'
-                />
-                <Step
-                  // active={active === 'Billing'}
-                  disabled={stage === 'Receipt' ? false : true}
-                  icon='file alternate outline'
-                  link
-                  onClick={this.handleClick}
-                  title='Receipt'
-                  description='Enter billing information'
-                />
-              </Step.Group>
+                  <Step.Group size='tiny' attached>
+                    <Step
+                      // active={active === 'Confirmation'}
+                      disabled={stage === 'Confirmation' ? false : true}
+                      icon='info'
+                      link
+                      onClick={this.handleClick}
+                      title='Confirm Order'
+                      description='Choose your shipping options'
+                    />
+                    <Step
+                      // active={active === 'Billing'}
+                      disabled={stage === 'Billing' ? false : true}
+                      icon='credit card'
+                      link
+                      onClick={this.handleClick}
+                      title='Billing'
+                      description='Download your receipt'
+                    />
+                    <Step
+                      // active={active === 'Billing'}
+                      disabled={stage === 'Receipt' ? false : true}
+                      icon='file alternate outline'
+                      link
+                      onClick={this.handleClick}
+                      title='Receipt'
+                      description='Enter billing information'
+                    />
+                  </Step.Group>
 
-              <Segment attached piled >
-                {stage === 'Confirmation' ?
-                  (
-                    <Container style={styles.itemsContainer} >
-                      <Grid centered>
-                        <Grid.Row>
-                          <Grid.Column textAlign='center'>
-                            <Header>Cart Items</Header>
-                          </Grid.Column>
-                        </Grid.Row>
-                      </Grid>
+                  <Segment attached piled >
+                    {stage === 'Confirmation' ?
+                      (
+                        <Container style={styles.itemsContainer} >
+                          <Grid centered>
+                            <Grid.Row>
+                              <Grid.Column textAlign='center'>
+                                <Header>Cart Items</Header>
+                              </Grid.Column>
+                            </Grid.Row>
+                          </Grid>
 
-                      <Table stackable>
-                        <Table.Header>
-                          <Table.Row>
-                            <Table.HeaderCell>Quantity</Table.HeaderCell>
-                            <Table.HeaderCell>Food Item</Table.HeaderCell>
-                            <Table.HeaderCell textAlign='right'>
-                              <Icon name='dollar' />Price
+                          <Table stackable>
+                            <Table.Header>
+                              <Table.Row>
+                                <Table.HeaderCell>Quantity</Table.HeaderCell>
+                                <Table.HeaderCell>Food Item</Table.HeaderCell>
+                                <Table.HeaderCell textAlign='right'>
+                                  <Icon name='dollar' />Price
                               </Table.HeaderCell>
-                          </Table.Row>
-                        </Table.Header>
-                        
-                        <Table.Body>
-                          {cartDetails.map((cartItem, index) => {
-                            return (
-                              <Table.Row key={index}>
-                                <Table.Cell>
-                                  {cartItem.quantity}
-                                </Table.Cell>
-                                <Table.Cell>
-                                  {cartItem.food.name}
-                                </Table.Cell>
-                                <Table.Cell textAlign='right'>
-                                  {cartItem.food.price * cartItem.quantity}
-                                </Table.Cell>
                               </Table.Row>
-                            )
-                          })}
-                        </Table.Body>
+                            </Table.Header>
 
-                        <Table.Footer>
-                          <Table.Row>
-                            <Table.HeaderCell>
-                              <List style={styles.totalList}>
-                                <List.Item>
-                                  <List.Content content='Subtotal' floated='left' />
-                                </List.Item>
-                                <List.Item>
-                                  <List.Content content='Tax' floated='left' />
-                                </List.Item>
-                                <List.Item>
-                                  <List.Content content='Total' floated='left' />
-                                </List.Item>
-                              </List>
-                            </Table.HeaderCell>
+                            <Table.Body>
+                              {cartDetails.map((cartItem, index) => {
+                                return (
+                                  <Table.Row key={index}>
+                                    <Table.Cell>
+                                      {cartItem.quantity}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                      {cartItem.food.name}
+                                    </Table.Cell>
+                                    <Table.Cell textAlign='right'>
+                                      {cartItem.food.price * cartItem.quantity}
+                                    </Table.Cell>
+                                  </Table.Row>
+                                )
+                              })}
+                            </Table.Body>
 
-                            <Table.HeaderCell />
+                            <Table.Footer>
+                              <Table.Row>
+                                <Table.HeaderCell>
+                                  <List style={styles.totalList}>
+                                    <List.Item>
+                                      <List.Content content='Subtotal' floated='left' />
+                                    </List.Item>
+                                    <List.Item>
+                                      <List.Content content='Tax' floated='left' />
+                                    </List.Item>
+                                    <List.Item>
+                                      <List.Content content='Total' floated='left' />
+                                    </List.Item>
+                                  </List>
+                                </Table.HeaderCell>
 
-                            <Table.HeaderCell >
-                              <List style={styles.totalList}>
-                                <List.Item>
-                                  <List.Content floated='right'>
-                                    {this.state.subTotal}
-                                  </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                  <List.Content floated='right'>
-                                    {this.state.tax}
-                                  </List.Content>
-                                </List.Item>
-                                <List.Item>
-                                  <List.Content floated='right'>
-                                    {total}
-                                  </List.Content>
-                                </List.Item>
-                              </List>
-                            </Table.HeaderCell>
+                                <Table.HeaderCell />
 
-                          </Table.Row>
-                        </Table.Footer>
-                      </Table>
+                                <Table.HeaderCell >
+                                  <List style={styles.totalList}>
+                                    <List.Item>
+                                      <List.Content floated='right'>
+                                        {this.state.subTotal}
+                                      </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                      <List.Content floated='right'>
+                                        {this.state.tax}
+                                      </List.Content>
+                                    </List.Item>
+                                    <List.Item>
+                                      <List.Content floated='right'>
+                                        {total}
+                                      </List.Content>
+                                    </List.Item>
+                                  </List>
+                                </Table.HeaderCell>
 
-                      <Grid>
-                        <Grid.Column textAlign="center">
-                          <Button positive animated='vertical' size='large' onClick={this.handleProceedOrder}>
-                            <Button.Content hidden>
-                              <Icon name='arrow right' />
-                            </Button.Content>
-                            <Button.Content visible>
-                              Proceed
+                              </Table.Row>
+                            </Table.Footer>
+                          </Table>
+
+                          <Grid>
+                            <Grid.Column textAlign="center">
+                              <Button positive animated='vertical' size='large' onClick={this.handleProceedOrder}>
+                                <Button.Content hidden>
+                                  <Icon name='arrow right' />
+                                </Button.Content>
+                                <Button.Content visible>
+                                  Proceed
                           </Button.Content>
-                          </Button>
-                        </Grid.Column>
-                      </Grid>
-                    </Container>
-                  ) : (stage === 'Billing' ? (
-                    <Grid centered>
-                      <Grid.Column mobile={16} tablet={9} computer={7} largeScreen={6}>
-                        <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
-                          <div className="example">
-                            <Elements>
-                              <CheckoutForm
-                                orderID={orderID}
-                                currentUser={currentUser}
-                                onPay={this.showReceipt}
-                              />
-                            </Elements>
-                          </div>
-                        </StripeProvider>
-                      </Grid.Column>
-                    </Grid>
-                  ) : (
-                      <Receipt
-                        cartDetails={this.props.cartDetails}
-                        receiptDetails={receiptDetails}
-                      />
-                    )
-                  )}
-              </Segment>
+                              </Button>
+                            </Grid.Column>
+                          </Grid>
+                        </Container>
+                      ) : (stage === 'Billing' ? (
+                        <Grid centered>
+                          <Grid.Column mobile={16} tablet={9} computer={7} largeScreen={6}>
+                            <StripeProvider apiKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
+                              <div className="example">
+                                <Elements>
+                                  <CheckoutForm
+                                    orderID={orderID}
+                                    currentUser={currentUser}
+                                    onPay={this.showReceipt}
+                                  />
+                                </Elements>
+                              </div>
+                            </StripeProvider>
+                          </Grid.Column>
+                        </Grid>
+                      ) : (
+                          <Receipt
+                            cartDetails={this.props.cartDetails}
+                            receiptDetails={receiptDetails}
+                          />
+                        )
+                      )}
+                  </Segment>
 
-            </Grid.Column>
-          </Grid.Row>
+                </Grid.Column>
+              </Grid.Row>
 
-        </Grid>
-      </Container >
+            </Grid>
+          </Container >
+        </div>
+        <Footer isInverted={true} />
+      </>
     )
   }
 }
